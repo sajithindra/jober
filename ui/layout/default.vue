aae58a23c7032e1f7d2a31254c9fc3fc6621039c
sajithsurendran <sajithsurendran007@gmail.com>
2023-02-06 01:52:01 +0530

    testing out the nuxt3

---
+++ ui/layout/default.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="fixed w-full bg-white shadow-md">
+      <div class="container mx-auto flex items-center p-6">
+        <a href="#" class="logo text-lg font-bold">App Name</a>
+        <nav class="ml-auto">
+          <NuxtLink class="px-4 py-2 text-gray-800 hover:bg-gray-200">Home</NuxtLink>
+          <NuxtLink class="px-4 py-2 text-gray-800 hover:bg-gray-200">Features</NuxtLink>
+        </nav>
+      </div>
+    </header>
+    <div>
+      <slot/>
+    </div>
+  </template>
---
+++ ui/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-center">Sajith </h1>
+</template>
