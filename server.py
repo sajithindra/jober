b0c3fa96b384d88138bc469661f5294b83d769a2
sajithsurendran <sajithsurendran007@gmail.com>
2023-03-01 16:01:17 +0530

    crud operation of the user

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from datetime import datetime
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 app = FastAPI()
@@ -18,7 +19,7 @@ class User(BaseModel):
     password : str
     created_at : str = datetime.now()
 
-@app.get('/user')
+@app.post('/user')
 async def create_user(user : User):
     try :
         client.jober.user.insert_one(dict(user))
@@ -48,7 +49,7 @@ class CUser(BaseModel):
     value : str
 
 @app.put('/user')
-async def change_user(user : Cuser):
+async def change_user(user : CUser):
     try: 
         filter = user.query
         update = {
@@ -62,3 +63,15 @@ async def change_user(user : Cuser):
         print(str(e))
         return False
 
+@app.delete('/user')
+async def delete_user(email:str):
+    try:
+        filter = {
+            'email' :email
+        }
+        client.jober.user.delete_one(filter=filter)
+        return True
+    except Exception as e:
+        print(str(e))
+        return False
+# user crud operations ends here
